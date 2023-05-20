f139210ee19283a87dfc74097a7dbce4496923b3
Larissa <110267129+larissambn@users.noreply.github.com>
2023-05-20 17:41:17 -0300

    Exercises almost done - missing 6 and 7

---
+++ Exercises/data_types_operators.py
@@ -0,0 +1,20 @@
+print('He said  "There are some who call me the Messiah"')
+print("He's is the Messiah, he's our savior")
+
+savings = 2500
+print("You have" + " " + str(savings) + " " +"in the bank")
+print("Your account has", savings, "euros") # coma is a possible way to concatenate integers in a sting phrase
+
+balance = 350
+deposit = input("Please enter the amount you wish to deposit : ")
+
+deposit = float(deposit)
+
+temp = 19.99
+
+temp = int(temp)
+
+print("The temparature is", temp, "degrees")
+
+#Exercise 6
+#Exercise 7
