b7119dab8eaeb6ed9c6e2bac7a6601871d5532f7
Larissa <110267129+larissambn@users.noreply.github.com>
2023-05-20 17:20:59 -0300

    first exercises

---
+++ Exercises/exercises_variables.py
@@ -0,0 +1,27 @@
+# Exercise 1
+
+print("Hello" + " " + "World")
+
+greating = "Ciao" 
+print (greating, "Bella") 
+
+# Exercise 2
+
+first_name = "Larissa"
+last_name = "Bueno"
+age = 26
+gender = "female"
+weight = 65.5
+diagnoses = ["sight" , "hair fall"]
+medication = ["glasses", "vitamins"]
+has_medical_card = False
+
+print(type(first_name))
+print (type(last_name))
+print (type(age))
+print (type(gender))
+print (type(weight)), 
+print (type(diagnoses)), 
+print(type(medication)),
+print(type(has_medical_card))
+
