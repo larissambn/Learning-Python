f12e72dbf56d19c5fc984a2ce988be58b498eaa4
Larissa <110267129+larissambn@users.noreply.github.com>
2023-05-31 12:24:33 -0300

    assessment done but incomplete

--- Exercises/assessment.py
+++ Exercises/assessment.py
@@ -1,6 +1,8 @@
 import random
 
+winner = 0 
 round = 1
+
 while round < 8:
         player_one_cards = [10,6,8,9,7,12,7]
         player_two_cards = [7,6,9,5,2,8,11]
@@ -8,13 +10,13 @@ while round < 8:
         card_player2 = (random.choice(player_two_cards))
         player1_win = 0
         player2_win = 0
-        
+
         print("Round" + " " + str(round) + " " + ":")
         if card_player1 > card_player2: 
-                player1_win += 1
+                player1_win =+ 1
                 print (" Player 1 wins the round! ", "with", card_player1, "beating", card_player2) 
         elif card_player2 > card_player1: 
-                player2_win += 1
+                player2_win =+ 1
                 print (" Player 2 wins the round! ", "with", card_player2, "beating", card_player1)
         else:
                 card_player1 == card_player2
@@ -22,13 +24,11 @@ while round < 8:
 
         round += 1
 
-if player1_win > player2_win:
-        print ( "Player 1 wins the game by", player1_win, "to", player2_win) 
-if player2_win > player1_win:
-        print ( "Player 2 wins the game by", player2_win, "to", player1_win) 
-else:("tie")               
+player1_win, player2_win
 
-                
+if player1_win> player2_win:
+        print ( "Player 1 wins the game by", player1_win, "to", player2_win) 
+if player2_win> player1_win:
+        print ( "Player 2 wins the game by", player2_win, "to", player1_win) 
+else : ("tie")               
         
-# Como executar If fora de função 
-# Como resgatar variável criada dentro de while loop.
