746d9791aa3569be3e11f3b9fa0ef844d31409af
Larissa <110267129+larissambn@users.noreply.github.com>
2023-09-05 06:58:51 -0300

    exercise 23 - lambda

---
+++ Daily_exercises/exercise_23.py
@@ -0,0 +1,5 @@
+user_input = input("Enter a number : ")
+n = int(user_input)
+
+x = lambda n : n ** 2
+print(x(n))
--- Daily_exercises/tempCodeRunnerFile.py
+++ Daily_exercises/tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-
-
-    print(tpls)
-
-for i in tpls:
-    name = i[0]
-    age = i[1]
-    score = i[2]
-
-    print(name, age, score)
-
+:
