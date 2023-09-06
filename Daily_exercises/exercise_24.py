51ec48c2582c0682aeeef674e9bfff9f3cd08638
Larissa <110267129+larissambn@users.noreply.github.com>
2023-09-06 05:58:57 -0300

    exercise 24 - biult in functions and documentation

---
+++ Daily_exercises/exercise_24.py
@@ -0,0 +1,13 @@
+print(eval.__doc__)
+
+def n_sum(a,b):
+    '''
+    Return the sum of a start value plus another number
+
+    The input numbers can be integers, strings, or float.
+    '''
+
+    return a + b
+
+print (n_sum(2,9))
+print (n_sum.__doc__)
